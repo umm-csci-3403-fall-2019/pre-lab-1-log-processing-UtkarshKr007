f18aa51297fc8b5407b28f0cdf37ec272d4245c6
UtkarshKr007 <kumar375@morris.umn.edu>
2019-09-04 03:29:58 -0500

    write wrap_contents.sh and create my_chart.html

---
+++ wrap_contents.sh
@@ -0,0 +1,11 @@
+# wrap_contents.sh takes 3 inputs:
+# The name of the file containing the "contents" that need to be wrapped between the header and footer
+content=$1
+# The common basename shared by the desired header and footer html files
+specifier=$2
+# The name of the output file
+output=$3
+
+# concatenates the content of the first argument between a header.html and footer.html specified by the second argument into a file
+#  named by the third argument
+cat "${specifier}_header.html" "${content}" "${specifier}_footer.html" > "${output}"
